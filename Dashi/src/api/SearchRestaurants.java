1d55f6aa0cf5f8266cfadce47f8e9c0cdf1d9efb
lung-zijim <lzylong@gmail.com>
2017-06-04 08:52:23 -0400

    Front-End

--- Dashi/src/api/SearchRestaurants.java
+++ Dashi/src/api/SearchRestaurants.java
@@ -13,6 +13,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.MySQLDBConnection;
+
 /**
  * Servlet implementation class SearchRestaurants
  */
@@ -33,19 +36,16 @@ public class SearchRestaurants extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		JSONArray array = new JSONArray();
-		try {
-			if (request.getParameterMap().containsKey("user_id")
-					&& request.getParameterMap().containsKey("lat")
-					&& request.getParameterMap().containsKey("lon")) {
-				String userId = request.getParameter("user_id");
-				double lat = Double.parseDouble(request.getParameter("lat"));
-				double lon = Double.parseDouble(request.getParameter("lon"));
-				// return some fake restaurants
-				array.put(new JSONObject().put("name", "Panda Express"));
-				array.put(new JSONObject().put("name", "Hong Kong Express"));
-			}
-		} catch (JSONException e) {
-			e.printStackTrace();
+		DBConnection connection = new MySQLDBConnection();
+		if (request.getParameterMap().containsKey("lat")
+				&& request.getParameterMap().containsKey("lon")) {
+			// term is null or empty by default
+			String term = request.getParameter("term");
+			//String userId = (String) session.getAttribute("user");
+			String userId = "1111";
+			double lat = Double.parseDouble(request.getParameter("lat"));
+			double lon = Double.parseDouble(request.getParameter("lon"));
+			array = connection.searchRestaurants(userId, lat, lon, term);
 		}
 		RpcParser.writeOutput(response, array);
 	}
